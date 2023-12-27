196a4b391da706d949dbe8f88581db2e4e7a0695
andrefcode <andrexfes08@gmail.com>
2023-12-26 21:32:00 -0300

    second commit

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,39 @@
+import java.util.Scanner;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
+
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-        
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.printf("Digite o primeiro número:");
+        Double valor1 = scanner.nextDouble();
+
+        System.out.printf("Digite o segundo número:");
+        Double valor2 = scanner.nextDouble();
+
+        System.out.printf("Digite a operação:");
+        String operacao = scanner.next();
+
+        Double resultado = calculadoraCompleta(valor1, valor2, operacao);
+        System.out.printf("Resultado:" + resultado);
     }
+
+
+    public static Double calculadoraCompleta(Double valor1, Double valor2, String operacao){
+
+        switch (operacao){
+            case "+":
+                return valor1 + valor2;
+            case "-":
+                return valor1 - valor2;
+            case "*":
+                return valor1 * valor2;
+            case "/":
+                return valor1 / valor2;
+
+        }
+        return 0.0;
+    }
+
 }
